1ff2ee630e7ff5e4d5c3ef87b7925b373887f1ca
Brian Turner <brianturnercs@gmail.com>
2024-03-23 22:36:19 -0600

    added movies CRUD

--- server.js
+++ server.js
@@ -86,6 +86,71 @@ router.post('/signin', function (req, res) {
     })
 });
 
+
+router.route('/movies')
+    .get(authJwtController.isAuthenticated, (req,res) => {
+        console.log(req.body);
+        //es = res.status(200);
+        if (req.get('Content-Type')) {
+            res = res.type(req.get('Content-Type'));
+        }
+        var o = getJSONObjectForMovieRequirement(req);
+        o.status = 200;
+        o.message = "GET movies";
+        o.query = o.body;
+        o.env = o.key;
+        res.json(o);
+    }
+    )
+    .post(authJwtController.isAuthenticated, (req,res) => {
+        console.log(req.body);
+        //es = res.status(200);
+        if (req.get('Content-Type')) {
+            res = res.type(req.get('Content-Type'));
+        }
+        var o = getJSONObjectForMovieRequirement(req);
+        o.status = 200;
+        o.message = "movie saved";
+        o.query = o.body;
+        o.env = o.key;
+        res.json(o);
+    }    
+    )
+    .delete(authController.isAuthenticated, (req, res) => {
+        console.log(req.body);
+        //es = res.status(200);
+        if (req.get('Content-Type')) {
+            res = res.type(req.get('Content-Type'));
+        }
+        var o = getJSONObjectForMovieRequirement(req);
+        o.status = 200;
+        o.message = "movie deleted";
+        o.query = o.body;
+        o.env = o.key;
+        res.json(o);
+    }
+
+    )
+    .put(authJwtController.isAuthenticated, (req, res) => {
+        console.log(req.body);
+        //es = res.status(200);
+        if (req.get('Content-Type')) {
+            res = res.type(req.get('Content-Type'));
+        }
+        var o = getJSONObjectForMovieRequirement(req);
+        o.status = 200;
+        o.message = "movie updated";
+        o.query = o.body;
+        o.env = o.key;
+        res.json(o);
+    }
+    )
+    .all((req, res) => {
+        res.status(405).send({ message: "HTTP method not supported."});
+    })
+    ;
+    
+
 app.use('/', router);
 app.listen(process.env.PORT || 8080);
 module.exports = app; // for testing only
